45026ac09e157f1e36935d197233bdf28bad5e98
Badshah  Yadav <96272309+BadshahYadav@users.noreply.github.com>
2021-12-29 16:49:14 +0530

    Update randam number generator.c

--- randam number generator.c
+++ randam number generator.c
@@ -5,7 +5,7 @@ int main() {
     int number;
     srand(time(0));
     number= rand()%1000+1;
-//randam number between (1-1000)
+//random number between (1-1000)
     printf("The number is %d\n", number);
 
     return 0;
